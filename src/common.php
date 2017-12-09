87d9b89f551f123e1e53df2603d8ecf2ac72439f
Yılmaz BAYSAL <yilmaz.baysal@technarts.com>
2017-12-09 21:36:43 +0300

    Login feature was implemented. A backgroud image is added to the project.

---
+++ src/common.php
@@ -0,0 +1,21 @@
+<?php
+// Different user types defined here to be use easily in code
+$USER_TYPE_ADMIN = 1;
+$USER_TYPE_BRANCH = 2;
+$USER_TYPE_MEMBER = 3;
+?>
+
+<html>
+    <head>
+        <style>
+            body {
+                background-image: url("static/background.png");
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+                background-size: cover;
+            }
+        </style>
+    </head>
+</html>
+
---
+++ src/login.php
@@ -0,0 +1,58 @@
+<?php
+include "common.php";
+include "connection.php";
+?>
+
+<html>
+<body>
+
+    <center>
+        <div class="login-form-container">
+            <form class="login-form" action="login.php" method="post">
+                <label>Username</label><br>
+                <input type="text" name="username" required><br><br>
+
+                <label>Password</label><br>
+                <input type="password" name="password" required><br><br>
+
+                <input type="submit" name="submit" value="Login">
+            </form>
+        </div>
+    </center>
+
+</body>
+</html>
+
+<?php
+
+if (isset($_POST['submit'])) {
+
+    $get_user_sql = oci_parse($connection,
+        'SELECT user_type_id as user_type, password FROM "USER" WHERE username = :username'
+    );
+    oci_bind_by_name($get_user_sql, ":username", $_POST['username']);
+
+    // Execute the SQL code
+    oci_execute($get_user_sql);
+
+    // Get the user
+    $user = oci_fetch_array($get_user_sql,OCI_ASSOC+OCI_RETURN_NULLS);
+
+    // Password control
+    if (!is_null($user) and password_verify($_POST['password'], $user['PASSWORD'])) {
+        echo "<center><p style='color: green'>You have successfully logged in.</p></center>";
+
+        // Save user data under the session
+        session_start();
+        $_SESSION['user_type'] = $user['USER_TYPE'];
+
+        // Redirect to home page
+        header("Location: index.php");
+        exit();
+    }
+    else {
+        echo "<center><p style='color: red'>Incorrect username password combination!</p></center>";
+    }
+
+}
+?>
